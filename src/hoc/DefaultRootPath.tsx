ae4786ae2e75fc56218841e6d85f8f2c3e83f0da
jin-Pro <dnjun2@ajou.ac.kr>
2023-10-05 15:12:06 +0900

    enhance: DefaultRootPath redirect default page

--- src/hoc/DefaultRootPath.tsx
+++ src/hoc/DefaultRootPath.tsx
@@ -2,6 +2,11 @@
 import { PropsWithChildren } from 'react';
 
 const DefaultRootPath = ({ children }: PropsWithChildren) => {
+  const defaultLanguage = window.location.pathname.split('/')[1];
+  if (defaultLanguage === '') {
+    window.location.replace('ko');
+    return null;
+  }
   return <>{children}</>;
 };
 
