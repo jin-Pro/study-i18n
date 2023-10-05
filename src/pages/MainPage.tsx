d4f354987c9772f81a08fd37a7e99873bc783642
jin-Pro <dnjun2@ajou.ac.kr>
2023-10-05 15:08:34 +0900

    feat: mainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,26 @@
+import { useTranslation } from '@i18n/.';
+
 const MainPage = () => {
-  return <div>안녕하세요</div>;
+  const { t, lang, changeLang } = useTranslation('translate');
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <h2>{t('greet')}</h2>
+      <button
+        onClick={() => changeLang(lang === 'ko' ? 'en' : 'ko')}
+        type='button'
+      >
+        Change Language
+      </button>
+    </div>
+  );
 };
 
 export default MainPage;
